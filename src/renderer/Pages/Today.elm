8faeb1f84d333c4abff9102b4820178c2a7159d9
Marei Kikukawa <contact@calmery.me>
2019-09-20 22:42:12 +0900

    feat: make a temporary layout

--- src/renderer/Pages/Today.elm
+++ src/renderer/Pages/Today.elm
@@ -1,7 +1,7 @@
 module Pages.Today exposing (view)
 
-import Html exposing (Html, a, main_, text)
-import Html.Attributes exposing (alt, href, src, style)
+import Html exposing (Html, a, div, h1, h2, main_, text)
+import Html.Attributes exposing (alt, class, href, src, style)
 import Model exposing (Model)
 import Update exposing (Msg)
 
@@ -9,5 +9,73 @@ import Update exposing (Msg)
 view : Model -> Html Msg
 view model =
     main_ []
-        [ a [ href "/#/all" ] [ text "All" ]
+        [ h1
+            []
+            [ text "Today" ]
+        , div [ class "partition" ]
+            [ div [ class "header" ]
+                [ div [ class "toggle" ] [ text "û" ]
+                , h2
+                    []
+                    [ text "Ready" ]
+                ]
+            , div
+                [ class "todo expired" ]
+                [ div [ class "strike" ] []
+                , div [ class "warning" ] [ text "Expired" ]
+                , div [ class "text" ] [ text "Reply to an email" ]
+                , div [ class "priority" ] [ text "è" ]
+                ]
+            , div
+                [ class "todo" ]
+                [ div [ class "strike" ] []
+                , div [ class "warning" ] [ text "Expired" ]
+                , div [ class "text" ] [ text "Take the minutes" ]
+                , div [ class "priority" ] [ text "è" ]
+                ]
+            , div
+                [ class "todo" ]
+                [ div [ class "strike" ] []
+                , div [ class "warning" ] [ text "Expired" ]
+                , div [ class "text" ] [ text "Write a blog" ]
+                , div [ class "priority" ] [ text "è" ]
+                ]
+            ]
+        , div [ class "partition" ]
+            [ div [ class "header" ]
+                [ div [ class "toggle" ] [ text "û" ]
+                , h2
+                    []
+                    [ text "Doing" ]
+                ]
+            , div
+                [ class "todo" ]
+                [ div [ class "strike" ] []
+                , div [ class "warning" ] [ text "Expired" ]
+                , div [ class "text" ] [ text "Make meeting materials" ]
+                , div [ class "priority" ] [ text "è" ]
+                ]
+            ]
+        , div [ class "partition" ]
+            [ div [ class "header" ]
+                [ div [ class "toggle" ] [ text "û" ]
+                , h2
+                    []
+                    [ text "Done" ]
+                ]
+            , div
+                [ class "todo done" ]
+                [ div [ class "strike" ] []
+                , div [ class "warning" ] [ text "Expired" ]
+                , div [ class "text" ] [ text "Call a business partner" ]
+                , div [ class "priority" ] [ text "è" ]
+                ]
+            , div
+                [ class "todo expired done" ]
+                [ div [ class "strike" ] []
+                , div [ class "warning" ] [ text "Expired" ]
+                , div [ class "text" ] [ text "Set up new desktop computer" ]
+                , div [ class "priority" ] [ text "è" ]
+                ]
+            ]
         ]
